a0061553d03035ce3369d97c8f00032819af6083
Luke Blondin <lblondin747@gmail.com>
2021-08-05 11:44:41 -0500

    initial commit

---
+++ LINQExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var VidGames = new List<string>() { "Fortnite", "Call of Duty", "Battlefield", "GTA", "Fall Guys", "Rocket League","Minecraft" };
+            var OrList = VidGames.OrderBy(x => x.Length);
+            foreach (var a in OrList)
+                Console.WriteLine(a);
+
+        }
+    }
+}
